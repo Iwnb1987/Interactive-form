8a43cc39231392b654cf9b6e84e66359f7304872
Iwnb1987 <149844666+Iwnb1987@users.noreply.github.com>
2024-01-31 14:54:28 -0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-  //Adding a name variable to manipulate later
+  //Adding input types to manipulate later
   const nameInput = document.getElementById('name');
   const eField = document.querySelector('input[name=user-name]').focus();
 
@@ -18,6 +18,8 @@
               jobRole.style.display = 'none';
             });
 
+
+
 //DOM manipulation Variables
 const bluePrint = document.getElementById('design');
 const shade = document.getElementById('color');
@@ -29,20 +31,22 @@ bluePrint.addEventListener('change', (e) => {
   for(let i = 0; i < shadeChildren.length; i++) {
     const dataShade = e.target.value;
     const dataColor = shadeChildren[i].getAttribute('data-theme');  //When the bluePrint variable experienes a change
- if(dataShade === dataColor) {                                      //it'll check lines 33 and 34 variables match
+ if(dataShade === dataColor) {                                      //It'll check if lines 33 and 34 variables match
   shadeChildren[i].hidden = false;                                  //since they do, when you switch designs, the JS pun theme
   shadeChildren[i].selected = true;                                 //won't display heart JS themes, vice versa.
- } else {
+  } else {
    shadeChildren[i].hidden = true;
    shadeChildren[i].selected = false;
- }
   }
+    }
 
-});
-          
+    });
+ 
 
-//DOM Manipulation variables
-const checkBox = document.querySelectorAll('input[type=checkbox]');
+ 
+
+       //DOM Manipulation variables
+  const checkBox = document.querySelectorAll('input[type=checkbox]');
   const checker = document.querySelector('#activities');
   const total = document.getElementById('activities-cost');
 
@@ -65,10 +69,10 @@ const cC = document.querySelector('.credit-card');
 const pay = document.getElementById('paypal').style.display = 'none';
 const payment = document.querySelector('#payment');
 const crypto = document.getElementById('bitcoin').style.display = 'none';
-payment.value = 'credit-card';  //the page did refresh after I filled out everything thanks to this line
+payment.value = 'credit-card';  //the page refreshes after credit card, name, and emailis filled in and submitted
 
 //this change will display effect the i'm going to pay with drop down menu
-//and hide the credit card tet field if paypal or bitcoin are selected
+//and hide the credit card text field if paypal or bitcoin are selected
 payment.addEventListener('change', (e) => {
 const divElement = e.target.value;
 if (divElement === 'credit-card') {
@@ -88,39 +92,45 @@ if (divElement === 'credit-card') {
   });
 
 
-//DOM variables to manipulate text fields
+ //DOM variables to manipulate text fields
 const email = document.querySelector('input[type=email]');
 const cardNo = document.querySelector('input[id=cc-num]');
 const zip = document.querySelector('input[id=zip]');
 const cvv = document.querySelector('input[id=cvv]');
-const form = document.getElementById('read');
+const form = document.querySelector('form[method=post]');
 
 //Before you can submit the form, these variables check for validation in the text fields
 form.addEventListener('submit', (e) => {
+
  const isNameValid = () => /^[a-zA-Z]+ ?[a-zA-Z]*? ?[a-zA-Z]*?$/.test(nameInput.value); 
  const isEmailValid = () => /^[^@]+@[^@.]+\.[a-z]+$/i.test(email.value);
  const isCreditLegit = () => /^\d{13,16}$/.test(cardNo.value);
  const isZipValid = () => /^\d{5}$/.test(zip.value);
  const isCvvValid = () => /^\d{3}$/.test(cvv.value);
 
- //This awesome function checks for correct format in the text field 
+      //This awesome function checks for correct format in the text field 
 const validator = (inputElement, validationFunction) => {
-if(validationFunction()) {
-  inputElement.closest('label').className = 'valid';
-  inputElement.nextElementSibling.style.display = 'none';
+  if(validationFunction()) {
+    inputElement.closest('label').className = 'valid';
+    inputElement.nextElementSibling.style.display = 'none';
+      } else {
+    e.preventDefault();
+    inputElement.closest('label').className = 'error';
+    inputElement.nextElementSibling.style.display = 'block';
+      }
+      };
+      
+    if(payment.value === 'credit-card') {
+      validator(cardNo, isCreditLegit);
+      validator(zip, isZipValid);
+      validator(cvv, isCvvValid);
     } else {
-  e.preventDefault();
-  inputElement.closest('label').className = 'error';
-  inputElement.nextElementSibling.style.display = 'block';
+      validator(nameInput, isNameValid);  //if nameInput.value matches isNameValid, a green checkmark will appear if you hit enter, etc.
+      validator(email, isEmailValid);
     }
-    };
-  validator(nameInput, isNameValid);  //if nameInput.value matches isNameValid, a green checkmark will appear if you hit enter, etc.
-  validator(email, isEmailValid);
-  validator(cardNo, isCreditLegit);
-  validator(zip, isZipValid);
-  validator(cvv, isCvvValid);
-    });
-
+    
+      });
+  
 
 //focuses and blurs the activity checkboxes on click
  const forBox = checkBox;
@@ -141,7 +151,8 @@ forBox[i].addEventListener('blur', (e) => {
      if(e.target.type ==='checkbox') {
         const activityName = e.target.name;
         const activityTimes = e.target.getAttribute("data-day-and-time");
-        //loops all of the activities to check if any have the same day and time.
+        //loops all of the activities to check if any have the same day and time. 
+        //And prevents selecting activities that match
         for(let i = 0; i < forBox.length; i++) {
           const dayTime = forBox[i].getAttribute("data-day-and-time");
         if(activityTimes === dayTime && activityName !== forBox[i].name) {
@@ -152,4 +163,3 @@ forBox[i].addEventListener('blur', (e) => {
         }
        }
       });
-    
